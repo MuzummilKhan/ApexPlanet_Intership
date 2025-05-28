1a596cce168a3f3ef79dd0f533775ad01454821b
Muzummil Khan <muzmmil809@gmail.com>
2025-05-28 08:14:13 +0530

    Add files via upload

---
+++ apex_task1.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const myButton = document.getElementById("myButton");
+
+  myButton.addEventListener("click", function () {
+    alert("yayyy you are a APEXPLANET intern");
+    console.log("Button was clicked at:", new Date());
+  });
+});
